7b543671eeed9a51b39cacb675084eb0e00e1241
Albert <72774907+AlbertSimaoS@users.noreply.github.com>
2025-02-01 15:57:47 -0300

    Update CalculadoraSalaRetangular.java

--- src/CalculadoraSalaRetangular.java
+++ src/CalculadoraSalaRetangular.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+public class CalculadoraSalaRetangular implements CalculoGeometrico{
 
-public class CalculadoraSalaRetangular {
+    @Override
+    public void calcularArea(double altura, double largura) {
+        double area = altura * largura;
+        System.out.println("a área do retângulo é: " + area);
+    }
+
+    @Override
+    public void calcularPerimetro(double altura, double largura) {
+        double perimetro = (altura * 2) + (largura * 2);
+        System.out.println("o perimêtro do retângulo é: " + perimetro);
+    }
 }
